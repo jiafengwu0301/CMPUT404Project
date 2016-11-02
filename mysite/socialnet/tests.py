da6b5d624128fb40605f843b347ad29af3c2f516
PreviousHippo <hty_1992@hotmail.com>
2016-11-02 13:43:45 -0600

    Asdqwe

--- mysite/socialnet/tests.py
+++ mysite/socialnet/tests.py
@@ -1,9 +1,9 @@
-# from django.test import TestCase
-# #create your test here
-# # from __future__ import unicode_literals
-# from django.contrib.auth.models import User
-#
-# from socialnet.models import *
+#create your test here
+from django.test import TestCase
+# from __future__ import unicode_literals
+from django.contrib.auth.models import User
+
+from socialnet.models import *
 # #
 #  class PostsTests(TestCase):
 # #
@@ -41,46 +41,46 @@
 #         model = Comment()
 #         self._plaintext_safe(model)
 #         self.is_markdown(model)
-#
-# class AuthorsTest(TestCase):
-#     def setUp(self):
-#         # create user profile, the format is: username, email, password
-#         self.user1 = User.objects.create_user('user1', 'user1@email.com', 'user1')
-#         self.user2 = User.objects.create_user('user2', 'user2@email.com', 'user2')
-#         self.author1 = Author(user = self.user1)
-#         self.author1.save()
-#         self.author2 = Author(user = self.user2)
-#         self.author2.save()
-#
-#     def test_create(self):
-#         #should create users
-#         self.assertTrue(isinstance(self.author1, Author))
-#         #cannot find the id for user
-#         #self.assertTrue(self.author1.)
-#         self.assertQuerysetEqual(self.author1.friends.all(), [])
+
+class AuthorsTest(TestCase):
+    def setUp(self):
+        # create user profile, the format is: username, email, password
+        self.user1 = User.objects.create_user('user1', 'user1@email.com', 'user1')
+        self.user2 = User.objects.create_user('user2', 'user2@email.com', 'user2')
+        self.author1 = Author(user = self.user1)
+        self.author1.save()
+        self.author2 = Author(user = self.user2)
+        self.author2.save()
+
+    def test_create(self):
+        #should create users
+        self.assertTrue(isinstance(self.author1, Author))
+        #cannot find the id for user
+        #self.assertTrue(self.author1.)
+        self.assertQuerysetEqual(self.author1.friends.all(), [])
 
 #==========================================================================>
 #======================NEW WORK, START OVER================================>
 #==========================================================================>
 #should test rest framework instead of the database
 
-from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-
-from .serializers import AuthorSerializer
-
-class CreateUserTest(APITestCase):
-    urls = 'mysite.urls'
-    def setUp(self):
-        self.superuser = User.objects.create_superuser('admin', 'admin123', 'admin@email.com')
-        self.assertTrue(self.superuser.is_staff)
-        # self.assertTrue(self.admin.is_admin)
-        self.assertTrue(self.superuser.is_superuser)
-        self.superuser.save()
-        self.client.login(username = 'admin', password = 'admin123')
-        self.data = {'username': 'mike', 'first_name': 'Mike', 'last_name': 'Tyson'}
-    def test_can_create_user(self):
-        repsonse = self.client.post(reverse('user-list'), self.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+# from django.contrib.auth.models import User
+# from django.core.urlresolvers import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+#
+# from .serializers import AuthorSerializer
+#
+# class CreateUserTest(APITestCase):
+#     urls = 'mysite.urls'
+#     def setUp(self):
+#         self.superuser = User.objects.create_superuser('admin', 'admin123', 'admin@email.com')
+#         self.assertTrue(self.superuser.is_staff)
+#         # self.assertTrue(self.admin.is_admin)
+#         self.assertTrue(self.superuser.is_superuser)
+#         self.superuser.save()
+#         self.client.login(username = 'admin', password = 'admin123')
+#         self.data = {'username': 'mike', 'first_name': 'Mike', 'last_name': 'Tyson'}
+#     def test_can_create_user(self):
+#         repsonse = self.client.post(reverse('user-list'), self.data)
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
