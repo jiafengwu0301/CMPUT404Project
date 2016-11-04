5efe9a366840a46bfb43a19dac44b822536da3c7
PreviousHippo <hty_1992@hotmail.com>
2016-11-03 22:51:42 -0600

    finally bb

--- mysite/socialnet/tests.py
+++ mysite/socialnet/tests.py
@@ -1,85 +1,54 @@
-# from django.test import TestCase
-# #create your test here
-# # from __future__ import unicode_literals
+#
 # from django.contrib.auth.models import User
+# from django.core.urlresolvers import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
 #
-# from socialnet.models import *
-# #
-#  class PostsTests(TestCase):
-# #
-# #     def _plaintext_safe(self, model_to_test):
-# #
-# #
-# #         """ Test if plaintext is safe: converts < > to &lt; &gt; etc.
-# #         Additionally ignores \n it does not convert that to <br/>
-# #         because the templates which use "linebreaksbr" already take care of that.
-# #         """
-# #         test_content = '#testing<b>\nno'
-# #         test_expect = '#testing&lt;b&gt;<br/>no'
-# #         model = model_to_test
-# #         model.content = test_content
-# #         model.markdown = False
-# #         #idk, but here shouldnt be models.__str__ cannot found the retreive for
-# #         #display post parts
-# #         self.assertEqual(models.__str__(), test_expect)
-# #
-# #     def is_markdown(self, model_to_test):
-# #         test_content = '# testing\n<b>\n>test-block'
-# #         test_expect = '<h1>testing</h1><br/><p>&lt;b&gt;</p><br/><blockquote><br/><p>test-block</p><br/></blockquote><br/>'
-# #         model = model_to_test
-# #         model.content = test_content
-# #         model.markdown = True
-# #         self.assertEqual(models.__str__(), test_expect)
-# #         self.assertEqual(models.content,test_content)
-# #
-#     def test_posts(self):
-#         model = Post()
-#         self.model
-#         self.is_markdown(model)
+# from .serializers import AuthorSerializer
 #
-#     def test_comments(self):
-#         model = Comment()
-#         self._plaintext_safe(model)
-#         self.is_markdown(model)
-#
-# class AuthorsTest(TestCase):
+# class CreateUserTest(APITestCase):
 #     def setUp(self):
-#         # create user profile, the format is: username, email, password
-#         self.user1 = User.objects.create_user('user1', 'user1@email.com', 'user1')
-#         self.user2 = User.objects.create_user('user2', 'user2@email.com', 'user2')
-#         self.author1 = Author(user = self.user1)
-#         self.author1.save()
-#         self.author2 = Author(user = self.user2)
-#         self.author2.save()
-#
-#     def test_create(self):
-#         #should create users
-#         self.assertTrue(isinstance(self.author1, Author))
-#         #cannot find the id for user
-#         #self.assertTrue(self.author1.)
-#         self.assertQuerysetEqual(self.author1.friends.all(), [])
-
-#==========================================================================>
-#======================NEW WORK, START OVER================================>
-#==========================================================================>
-#should test rest framework instead of the database
-
+#         self.superuser = User.objects.create_superuser('admin', 'admin123', 'admin@email.com')
+#         self.assertTrue(self.superuser.is_staff)
+#         # self.assertTrue(self.admin.is_admin)
+#         self.assertTrue(self.superuser.is_superuser)
+#         self.superuser.save()
+#         self.client.login(username = 'admin', password = 'admin123')
+#         self.data = {'username': 'mike', 'first_name': 'Mike', 'last_name': 'Tyson'}
+#     def test_can_create_user(self):
+#         repsonse = self.client.post(reverse('user-list'), self.data)
+#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+from django.test import TestCase
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from socialnet.models import *
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from .viewsauthors import *
+from .viewsposts import *
 
-from .serializers import AuthorSerializer
+class AuthorApiTest(APITestCase):
 
-class CreateUserTest(APITestCase):
-    def setUp(self):
-        self.superuser = User.objects.create_superuser('admin', 'admin123', 'admin@email.com')
-        self.assertTrue(self.superuser.is_staff)
-        # self.assertTrue(self.admin.is_admin)
-        self.assertTrue(self.superuser.is_superuser)
-        self.superuser.save()
-        self.client.login(username = 'admin', password = 'admin123')
-        self.data = {'username': 'mike', 'first_name': 'Mike', 'last_name': 'Tyson'}
-    def test_can_create_user(self):
-        repsonse = self.client.post(reverse('user-list'), self.data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_create_user(self):
+        factory = APIRequestFactory()
+        data = {
+            "username": "absdbasbdab",
+            "password": "sdsad12ad",
+            "email": "sdanbv@email.com",
+            "first_name": "asdgqwe",
+            "last_name": "asdggqw",
+            "github": "asdkjvc@github.com",
+            "avatar": "null"
+        }
+        request = factory.post('/socialnet/authors/create/', data)
+        view = AuthorCreateView.as_view()
+        response = view(request)
+
+
+    def test_create_post(self):
+        factory = APIRequestFactory()
+        data = {
+            "detail": "i love big mac"
+        }
+        request = factory.post('/socialnet/posts/create', data)
+        view = PostCreateView.as_view()
+        response = view(request)
