620f7beac4b5652d1cef1aa66be034f1fdd46851
PreviousHippo <hty_1992@hotmail.com>
2016-11-03 23:35:53 -0600

    works

--- mysite/socialnet/tests.py
+++ mysite/socialnet/tests.py
@@ -25,6 +25,8 @@ class AuthorApiTest(APITestCase):
         response = view(request)
 
 
+class PostApiTest(APITestCase):
+
     def test_create_post(self):
         factory = APIRequestFactory()
         data = {
@@ -32,4 +34,36 @@ class AuthorApiTest(APITestCase):
         }
         request = factory.post('/socialnet/posts/create', data)
         view = PostCreateView.as_view()
+        response = view(request)
+
+
+    def test_delete_post(self):
+        factory = APIRequestFactory()
+        request = factory.delete('socialnet/posts')
+        view = PostDestroyView.as_view()
+        response = view(request)
+
+
+    def test_get_post(self):
+        factory = APIRequestFactory()
+        request = factory.get('/socialnet/posts')
+        view = PostListView.as_view()
+        response = view(request)
+
+
+class CommentsApiTest(APITestCase):
+    def test_create_comment(self):
+        factory = APIRequestFactory()
+        data = {
+            "detail": "go to bed now"
+        }
+        request = factory.post('/socialnets/posts/comments', data)
+        view = CommentCreateView.as_view()
+        response = view(request)
+
+
+    def test_get_comment(self):
+        factory = APIRequestFactory()
+        request = factory.get('/socialnets/posts/comments')
+        view = CommentListView.as_view()
         response = view(request)
