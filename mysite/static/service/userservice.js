8141a9e5f1560a9dda0b12b885cbf4e05989ebf5
jiafengwu0301 <jiafengwu0301@gmail.com>
2016-11-22 23:03:02 -0700

    Change host uri

--- mysite/static/service/userservice.js
+++ mysite/static/service/userservice.js
@@ -32,24 +32,27 @@ function userService($http,$rootScope,$location,$cookies) {
 
     return service;
 
+    var host_heroku = "socialnets404.herokuapp.com";
+    var host_heroku_local = "127.0.0.1:8000";
+
     // create a user
     function createUser(author){
-        return $http.post('http://127.0.0.1:8000/socialnet/authors/create/',author).then(handleSuccess, handleError('Error'));
+        return $http.post('http://'+host_heroku+'/socialnet/authors/create/',author).then(handleSuccess, handleError('Error'));
     }
 
     // update author's informations
     function updateAuthor(id, update){
-        return $http.put('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/'+id+'/update/', update)
+        return $http.put('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/'+id+'/update/', update)
     }
 
     // get all posts that current use has permission
     function getAllPost(){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/posts/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/posts/').then(handleSuccess, handleError('Error'));
     }
 
     // get all author
     function getAllAuthor(){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/')
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/')
     }
 
     // get Github avtivity
@@ -59,79 +62,79 @@ function userService($http,$rootScope,$location,$cookies) {
 
     // get post by id
     function getPost(id){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/'+id+'/posts/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/'+id+'/posts/').then(handleSuccess, handleError('Error'));
     }
 
     // make a new post for current user
     function newPost(post){
         var a = JSON.stringify(post);
-        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/posts/create/',JSON.parse(a)).then(handleSuccess, handleError('Error'));
+        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/posts/create/',JSON.parse(a)).then(handleSuccess, handleError('Error'));
     }
 
     // delete a post by id
     function deletePost(id){
-        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/posts/'+id+'/destroy/');
+        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/posts/'+id+'/destroy/');
     }
 
     // edit a post by id and the post with edit
     function editPost(id, post){
         var a = JSON.stringify(post);
-        return $http.put('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/posts/'+id+'/update/', JSON.parse(a));
+        return $http.put('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/posts/'+id+'/update/', JSON.parse(a));
     }
 
     // get all friends by author id
     function getAllMyFriend(id){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/'+id+'/network/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/'+id+'/network/').then(handleSuccess, handleError('Error'));
     }
 
     // get friend's post by friend's id
     function getFriendPosts(id){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/'+id+'/posts/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/'+id+'/posts/').then(handleSuccess, handleError('Error'));
     }
 
     // get an author by its id
     function getAuthorById(id){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/'+id+'/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/'+id+'/').then(handleSuccess, handleError('Error'));
     }
 
     // get current author's information
     function getAuthorForAuthentication(id,username,password){
-        return $http.get('http://'+username+':'+password+'@127.0.0.1:8000/socialnet/authors/'+id+'/').then(handleSuccess, handleError('Error'));
+        return $http.get('http://'+username+':'+password+'@'+host_heroku+'/socialnet/authors/'+id+'/').then(handleSuccess, handleError('Error'));
     }
 
     // make a new comment by post id and comment data
     function newComment(id,comment){
-        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/posts/'+id+'/comments/create/',comment).then(handleSuccess, handleError('Error'));
+        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/posts/'+id+'/comments/create/',comment).then(handleSuccess, handleError('Error'));
     }
 
     // delete a comment by its id
     function deleteComment(id){
-        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/comments/'+id+'/destroy/').then(handleSuccess, handleError('Error'));
+        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/comments/'+id+'/destroy/').then(handleSuccess, handleError('Error'));
     }
 
     // unfriend with an author
     function removeFriend(id){
-        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/friends/unfriend/'+id+'/')
+        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/friends/unfriend/'+id+'/')
     }
 
     // send a friend request to an author
     function sendFriendRequest(id){
-        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/friend_request/'+id+'/')
+        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/friend_request/'+id+'/')
     }
 
     // get all request you send and receive
     function request(){
-        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/friends/friend_requests/')
+        return $http.get('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/friends/friend_requests/')
     }
 
     // accept a friend request
     function acceptRequest(id){
-        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/friend_request/accept/'+id+'/')
+        return $http.post('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/friend_request/accept/'+id+'/')
     }
 
     // reject a friend request
     function rejectRequest(id){
-        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@127.0.0.1:8000/socialnet/authors/friend_request/reject/'+id+'/')
+        return $http.delete('http://'+Base64.decode($rootScope.globals.currentUser.authdata)+'@'+host_heroku+'/socialnet/authors/friend_request/reject/'+id+'/')
     }
 
     // if success, return the data
