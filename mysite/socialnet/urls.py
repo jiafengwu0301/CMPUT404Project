efff8242a16e79d0b72e76d58afcd81413f49848
Edson Gustavo Santiago Silva <egustavo4@gmail.com>
2016-11-26 18:36:48 -0700

    authors url changed to author/

--- mysite/socialnet/urls.py
+++ mysite/socialnet/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
 	url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 	url(r'^posts/', include('socialnet.urlsposts')),
-	url(r'^authors/', include('socialnet.urlsauthors')),
+	url(r'^author/', include('socialnet.urlsauthors')),
 	url(r'^comments/', include('socialnet.urlscomments')),
 	url(r'^auth/', viewsauthors.AuthorAuthenticationView.as_view(), name='auth'),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
